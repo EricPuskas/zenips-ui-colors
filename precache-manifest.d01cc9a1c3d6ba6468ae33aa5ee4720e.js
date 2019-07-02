189c5a48265fa3f465f300a1950253889bbb4716
unknown <eric.puskas@yahoo.com>
2019-07-02 21:04:05 +0300

    Updates

---
+++ precache-manifest.d01cc9a1c3d6ba6468ae33aa5ee4720e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5ec174321c98e0a715db22534b5dee",
+    "url": "/zenips-ui-colors/index.html"
+  },
+  {
+    "revision": "7ed1c63bf00053987c48",
+    "url": "/zenips-ui-colors/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "66b4d25c29dcee127351",
+    "url": "/zenips-ui-colors/static/css/main.e964a052.chunk.css"
+  },
+  {
+    "revision": "7ed1c63bf00053987c48",
+    "url": "/zenips-ui-colors/static/js/2.23b71ca6.chunk.js"
+  },
+  {
+    "revision": "66b4d25c29dcee127351",
+    "url": "/zenips-ui-colors/static/js/main.dc70c7a8.chunk.js"
+  },
+  {
+    "revision": "ba05bb8ad5db9557d568",
+    "url": "/zenips-ui-colors/static/js/runtime~main.ec73cb27.js"
+  },
+  {
+    "revision": "26e58f94ae00d1f051243be31d56579b",
+    "url": "/zenips-ui-colors/static/media/bg.26e58f94.svg"
+  }
+]);
